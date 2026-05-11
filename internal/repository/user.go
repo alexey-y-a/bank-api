31ed1af045f1472e17d460b2a4434ac6792d85bb
alexey-y-a <lexus2157@mail.ru>
2026-05-11 16:31:36 +0300

    step_014 done

---
+++ internal/repository/user.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/alexey-y-a/bank-api/internal/domain"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *domain.User) error
+	FindByEmail(ctx context.Context, email string) (*domain.User, error)
+	FindByID(ctx context.Context, id string) (*domain.User, error)
+}
