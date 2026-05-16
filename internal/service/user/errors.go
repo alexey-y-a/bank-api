a05a4a4369197390723c9253d0d47b7e2c491905
alexey-y-a <lexus2157@mail.ru>
2026-05-16 23:20:31 +0300

    step_017 done

---
+++ internal/service/user/errors.go
@@ -0,0 +1,9 @@
+package user
+
+import "errors"
+
+var (
+	ErrUserAlreadyExists  = errors.New("user with this email already exists")
+	ErrInvalidCredentials = errors.New("invalid email or password")
+	ErrUserNotFound       = errors.New("user not found")
+)
