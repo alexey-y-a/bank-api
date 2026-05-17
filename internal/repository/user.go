5c3ba0aa473ad04dcdf02f85257f56392dc42a21
alexey-y-a <lexus2157@mail.ru>
2026-05-17 21:30:21 +0300

    step_019 done

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -6,6 +6,8 @@ import (
 	"github.com/alexey-y-a/bank-api/internal/domain"
 )
 
+//go:generate mockgen -source=$GOFILE -destination=mocks/user_repo_mock.gen.go -package=mocks
+
 type UserRepository interface {
 	Create(ctx context.Context, user *domain.User) error
 	FindByEmail(ctx context.Context, email string) (*domain.User, error)
