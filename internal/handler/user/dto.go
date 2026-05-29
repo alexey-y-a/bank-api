1b4fd570fa63c6e673f71f89f55c0bce800ebc01
alexey-y-a <lexus2157@mail.ru>
2026-05-29 23:20:55 +0300

    step_025 done

--- internal/handler/user/dto.go
+++ internal/handler/user/dto.go
@@ -41,18 +41,22 @@ func toUserResponse(u *domain.User) UserResponse {
 }
 
 func (r RegisterRequest) Validate() error {
-	return validation.ValidateStruct(&r,
-		validation.Field(&r.Email,
+	return validation.ValidateStruct(
+		&r,
+		validation.Field(
+			&r.Email,
 			validation.Required.Error("email is required"),
 			is.EmailFormat.Error("invalid email format"),
 			validation.Length(5, 255).Error("email must be between 5 and 255 characters"),
 		),
-		validation.Field(&r.Username,
+		validation.Field(
+			&r.Username,
 			validation.Required.Error("username is required"),
 			validation.Length(3, 50).Error("username mast be between 3 and 50 characters"),
 			validation.Match(regexp.MustCompile(`^[a-zA-Z0-9_]+$`)).Error("username can only contain letters, digits and underscores"),
 		),
-		validation.Field(&r.Password,
+		validation.Field(
+			&r.Password,
 			validation.Required.Error("password is required"),
 			validation.Length(8, 128).Error("password must be between 8 and 128 characters"),
 		),
@@ -60,11 +64,14 @@ func (r RegisterRequest) Validate() error {
 }
 
 func (r LoginRequest) Validate() error {
-	return validation.ValidateStruct(&r,
-		validation.Field(&r.Email,
+	return validation.ValidateStruct(
+		&r,
+		validation.Field(
+			&r.Email,
 			validation.Required.Error("email is required"),
 		),
-		validation.Field(&r.Password,
+		validation.Field(
+			&r.Password,
 			validation.Required.Error("password is required"),
 		),
 	)
